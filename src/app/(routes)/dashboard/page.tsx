b85c0dc1108d36d41bbf144ca332cf5f2e087ef1
cakfan <taufan.fhm8@gmail.com>
2024-06-24 08:12:42 +0700

    fix: suspense dashboard

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -6,13 +6,14 @@ import { formatNumber } from "@/lib/utils";
 import { Film, TrendingUp, User } from "lucide-react";
 import type { Metadata } from "next";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Dashboard",
   description: "Manage data on this website",
 };
 
-export default async function DashboardPage() {
+const DashboardComponent = async () => {
   const me = await getMe();
   if (!me) redirect("/");
   const isAdmin = me.role === "admin";
@@ -21,48 +22,56 @@ export default async function DashboardPage() {
   const reviews = await getAllReviews();
 
   return (
+    <div className="grid w-full gap-4 md:grid-cols-3">
+      {isAdmin && (
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Users</CardTitle>
+            <User className="text-omsetro h-4 w-4" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">
+              {formatNumber(users?.length ?? 0)}
+            </div>
+          </CardContent>
+        </Card>
+      )}
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Posts</CardTitle>
+          <Film className="text-omsetro h-4 w-4" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">
+            {formatNumber(posts?.length ?? 0)}
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Reviews</CardTitle>
+          <TrendingUp className="text-omsetro h-4 w-4" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">
+            {formatNumber(reviews?.length ?? 0)}
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default async function DashboardPage() {
+  return (
     <div className="flex min-h-screen w-full flex-col gap-10 p-10 lg:px-20">
       <div className="prose dark:prose-invert">
         <h1 className="mb-2">Dashboard</h1>
         <p className="my-0">Manage data on this website</p>
       </div>
-      <div className="grid w-full gap-4 md:grid-cols-3">
-        {isAdmin && (
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Users</CardTitle>
-              <User className="text-omsetro h-4 w-4" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">
-                {formatNumber(users?.length ?? 0)}
-              </div>
-            </CardContent>
-          </Card>
-        )}
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Posts</CardTitle>
-            <Film className="text-omsetro h-4 w-4" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">
-              {formatNumber(posts?.length ?? 0)}
-            </div>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Reviews</CardTitle>
-            <TrendingUp className="text-omsetro h-4 w-4" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">
-              {formatNumber(reviews?.length ?? 0)}
-            </div>
-          </CardContent>
-        </Card>
-      </div>
+      <Suspense fallback={<p>Loading data</p>}>
+        <DashboardComponent />
+      </Suspense>
     </div>
   );
 }
