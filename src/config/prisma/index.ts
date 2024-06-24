b877720d82f0d7874db42c4c702b229c44b5d6c1
cakfan <taufan.fhm8@gmail.com>
2024-06-24 16:51:08 +0700

    fix: prisma connection production

--- src/config/prisma/index.ts
+++ src/config/prisma/index.ts
@@ -1,10 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const prismaClientSingleton = () => {
+  const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
+  const adapter = new PrismaNeon(neon);
+  return new PrismaClient({ adapter });
+};
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: ReturnType<typeof prismaClientSingleton> | undefined;
 }
 
-const prismadb = globalThis.prisma || new PrismaClient();
+const prismadb = globalThis.prisma ?? prismaClientSingleton();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
 export default prismadb;
