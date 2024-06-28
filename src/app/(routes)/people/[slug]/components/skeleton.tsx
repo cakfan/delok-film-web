14a8d95faf30b25e286e71b63390a76fccf16960
cakfan <taufan.fhm8@gmail.com>
2024-06-29 06:49:43 +0700

    fix: detail skeleton

--- src/app/(routes)/people/[slug]/components/skeleton.tsx
+++ src/app/(routes)/people/[slug]/components/skeleton.tsx
@@ -1,32 +1,34 @@
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Skeleton } from "@/components/ui/skeleton";
 
-const PeopleSkeleton = () => {
+const PostSkeleton = () => {
   return (
     <div className="flex w-full flex-wrap justify-center px-10 py-14 md:gap-24 lg:gap-40 lg:px-20">
       <div className="flex flex-1 flex-col gap-8">
-        <Skeleton className="h-14 w-3/4 rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
+        <Skeleton className="h-10 w-3/4 rounded-md" />
+        <div className="mt-10 flex flex-col gap-4">
+          <Skeleton className="h-4 w-full rounded-md" />
+          <Skeleton className="h-4 w-3/4 rounded-md" />
+          <Skeleton className="h-4 w-1/2 rounded-md" />
+          <Skeleton className="h-4 w-2/3 rounded-md" />
+          <Skeleton className="h-4 w-full rounded-md" />
+          <Skeleton className="h-4 w-3/4 rounded-md" />
+          <Skeleton className="h-4 w-1/2 rounded-md" />
+          <Skeleton className="h-4 w-2/3 rounded-md" />
+          <Skeleton className="h-4 w-4/5 rounded-md" />
+          <Skeleton className="h-4 w-full rounded-md" />
+        </div>
       </div>
-      <div className="flex w-full flex-col md:w-1/4">
+      <div className="flex w-full flex-col gap-4 md:w-1/4">
         <AspectRatio ratio={9 / 16}>
-          <Skeleton className="w-full rounded-md" />
+          <Skeleton className="h-full w-full rounded-md" />
         </AspectRatio>
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
-        <Skeleton className="h-8 w-full rounded-md" />
+        <Skeleton className="mt-8 h-4 w-3/4 rounded-md" />
+        <Skeleton className="h-4 w-full rounded-md" />
+        <Skeleton className="h-4 w-full rounded-md" />
       </div>
     </div>
   );
 };
 
-export default PeopleSkeleton;
+export default PostSkeleton;
